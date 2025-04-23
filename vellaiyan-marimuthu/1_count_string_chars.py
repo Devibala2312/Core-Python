a8ba0463e91c9eabac01c63da11eecad882c18cb
Vellaiyan M <vellaiyan.m@ideas2it.com>
2025-04-23 18:54:31 +0530

    assignment-1

---
+++ vellaiyan-marimuthu/1_count_string_chars.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+print("Program to count the number of characters")
+string = input("Enter your string: ")
+string = string.lower()
+strCount = {}
+for v in string:
+    strCount[v] = strCount.get(v,0) + 1
+print("The character counts are (using for loop)", strCount)
+count = Counter(string)
+print ("The character counts are(using counter collection) ", dict(count))
